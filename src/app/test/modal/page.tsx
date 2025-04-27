1e1f44be9ad5a2edf3c4c13d04b88f00754243aa
gyuha <nicegyuha@gmail.com>
2025-04-27 17:57:25 +0900

    feat: 모달 컴포넌트 구조 개선 및 카드 레이아웃 추가

--- src/app/test/modal/page.tsx
+++ src/app/test/modal/page.tsx
@@ -1,25 +1,43 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { uuid } from '@/lib/utils';
-import useModal from '@/stores/modal-store';
-import type React from 'react';
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { uuid } from "@/lib/utils";
+import useModal from "@/stores/modal-store";
+import type React from "react";
 
 const Modal = (): React.JSX.Element | null => {
   const { openModal } = useModal();
 
-  return <Button
-  onClick={() => {
-    openModal({
-      title: `modal #${uuid()}`,
-      info: `test
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button
+          onClick={() => {
+            openModal({
+              title: `modal #${uuid()}`,
+              info: `test
 information`,
-      content: 'Hello, Modal!!!!',
-      size: 'lg',
-    });
-  }}
->
-  Open
-</Button>
+              content: "Hello, Modal!!!!",
+              size: "lg",
+            });
+          }}
+        >
+          Open
+        </Button>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Modal;
